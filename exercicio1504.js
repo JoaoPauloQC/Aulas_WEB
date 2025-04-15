2e52924c39c18825d43cad4a8e61644f7ec2ea23
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2025-04-15 16:18:52 -0300

    aula01

---
+++ exercicio1504.js
@@ -0,0 +1,17 @@
+let x = 10;
+let y = 3;
+
+console.log('x = ' + x);
+console.log('x = ' + y);
+console.log('-x = ' + (-x));
+console.log('-y = ' + (-y));
+console.log('x/y = ' + (x/y));
+console.log('x*y= ' + (x*y));
+console.log('x+y = ' + (x + y));
+console.log('x-y = ' + (x-y));
+console.log('Inteiro próximo de x/y = ' + (Math.floor(x/y)));
+console.log('x + 1 = ' + (++x));
+console.log('y+1 = ' + (++y));
+console.log('y = '+ y);
+console.log('x = ' + x);
+
