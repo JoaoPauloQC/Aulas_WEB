71ce054bce2e66d4bd528589af13749e8b89b93a
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2025-05-07 21:39:11 -0300

    Correção de bugs e funcionando

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
-let input = document.getElementById("numero")
-let num = input.value
+let input = document.getElementById("num")
+
 let mensagem = document.getElementById("msg")
 let botao = document.getElementById("send")
 
 
-let teste = 2
 
 
-meuBotao.addEventListener('click', function() {
 
-    if (perfect_num(num) == True){
-        mensagem.innerHTML("Número perfeito")
+botao.addEventListener('click', function (event) {
+
+    let num = input.value 
+
+    event.preventDefault()
+    if (perfect_num(num) == true) {
+        mensagem.classList.add("sucess")
+        mensagem.innerHTML = "Número perfeito"
+
     }
-    else{
-        mensagem.innerHTML("Número não perfeiyo")
+    else {
+        mensagem.innerHTML = "Número não perfeito"
+        mensagem.classList.add("non-sucess")
     }
-  });
+});
 
 
 
 
-function perfect_num(a){
+function perfect_num(a) {
 
-    i = 0
-    somadivisores = 0
-    while (i <= a) {
-        if (i % a == 0){
+    let i = 0
+    let somadivisores = 0
+    while (i < a) {
+        if (a % i == 0) {
             somadivisores = somadivisores + i
         }
-        
+        console.log(i)
+        console.log(somadivisores)
         i = i + 1
     }
 
-    if (somadivisores == a){
-        return True
+    if (somadivisores == a) {
+        return true
     }
     else {
-        return False
+        return false
     }
 
 }
-
-perfect_num(teste)
